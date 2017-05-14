6ededa74b6b2a0a00d087db5666f32953bb5fa86
Albert Lam <albert233@hotmail.com>
2017-05-15 01:26:02 +1000

    Create P16.scala

---
+++ P16.scala
@@ -0,0 +1,8 @@
+// P16.scala
+// Drop every Nth element from a list
+
+object P16 {
+	def drop[A](n: Int, l: List[A]): List[A] = {
+		l.zipWithIndex.map{case (k,v) => (k,v+1)}.filter(_._2 % n != 0).map(_._1)
+	}
+}
