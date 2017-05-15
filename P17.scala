9a974fe1da84b8abf5b3e0862d519aa1413ee8fa
Albert Lam <albert233@hotmail.com>
2017-05-16 00:17:50 +1000

    Update P17.scala

--- P17.scala
+++ P17.scala
@@ -2,5 +2,5 @@
 // Split a list into two parts
 
 object P17 {
-	def split[A](n: Int, l: List[A]): (List[A],List[A]) = l.splitAt(n)
+	def split[A](n: Int, l: List[A]): (List[A], List[A]) = l.splitAt(n)
 }
